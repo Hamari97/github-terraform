13725524eea391f307b26cdc7bf7d84a1aff9c8a
Hamari97 <mari.hammeraas@hotmail.com>
2023-10-18 19:50:06 +0200

    web

---
+++ web/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-mh"
+    storage_account_name = "sabetfs9fv5hum5rf"
+    container_name       = "tfstate"
+    key                  = "web-demo.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ web/variables.tf
@@ -0,0 +1,26 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the resource group"
+}
+
+variable "sa_name" {
+  type        = string
+  description = "Name of the Storage Account"
+
+}
+
+variable "source_content" {
+    type = string
+    description = "Source content for the index.html file"
+  
+}
+
+variable "index_document" {
+  type        = string
+  description = "Name of the index document"
+}
