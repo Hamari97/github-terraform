f17c148c9a72684ca650657377ac864d42da516d
Hamari97 <mari.hammeraas@hotmail.com>
2023-11-09 02:11:09 +0100

    deploy staticwebsite

---
+++ staticwebsite/variables.tf
@@ -0,0 +1,30 @@
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group"
+  default = "rg-web"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the resource group"
+  default = "westeurope"
+}
+
+variable "sa_name" {
+  type        = string
+  description = "Name of the Storage Account"
+  default = "saweb"
+}
+
+variable "source_content" {
+  type        = string
+  description = "Source content for the index.html file"
+  default = "<h1>Web page created with Terraform</h1>"
+}
+
+variable "index_document" {
+  type        = string
+  description = "Name of the index document"
+  default = "index.html"
+}
+
