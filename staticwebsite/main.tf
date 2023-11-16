7f9460159db4c206b210b0869573b666943da6a5
Hamari97 <mari.hammeraas@hotmail.com>
2023-11-17 00:02:36 +0100

    destroy

--- staticwebsite/main.tf
+++ staticwebsite/main.tf
@@ -39,7 +39,7 @@ resource "azurerm_storage_blob" "index_html" {
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
-  source_content         = "${var.source_content}${local.workspaces_suffix}"
+  source_content         = "${var.source_content}${local.web_suffix}"
 }
 
 output "primary_web_endpoint" {
